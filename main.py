105492c00def300055bba7177e23721a2ceceab9
Jason Buquiran <jbuq319@UoA.auckland.ac.nz>
2020-04-23 20:02:39 +1200

    added code for main and fixed image_process
    -the file for image_process was not a python file and changed the file
    -added code for the main.py file that lets us run the code by just running main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-test
+from image_process import emotions
+from trainAlgorithm import trainer
+import sys
+
+sys.path.insert(1, 'models')
+
+#Constants
+REBUILD_DATA = False
+IMAGE_SIZE = 0
+EPOCHS = 0
+BATCH_SIZE = 0
+folderName = ""
+fileName = "AlexNet-Data"
+
+
+#Save and Load Data
+
+
+
+#Augment the Data to suite the
+training_data = ip.FlipImage(training_data)
+training_data = ip.RandCrop(training_data, 5, (227,227))
+training_data = ip.ConvertToTensor(training_data)
+
+#Training the Data
+train = trainer()
+
+train.start(EPOCHS, BATCH_SIZE)
+train.result()
+train.saveResult("AlexNet-Results")
