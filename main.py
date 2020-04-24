05da3f86698f0d16db93d5b4f02a5f536f65e641
Jason Buquiran <jbuq319@UoA.auckland.ac.nz>
2020-04-25 04:11:55 +1200

    Added Comments on the Template of the main.py
    -Added the structure of the main.py of the process of the structure
    
    -Still need to finish certain stuff for it to get ready for testing of the AlexNet

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-test
+from image_processing import dataLoader, emotions
+from training import trainer
+
+import sys
+
+sys.path.insert(1, 'models')
+
+from AlexNet import AlexNet
+
+# Constants
+EPOCHS = 1
+BATCH_SIZE = 1
+IMAGE_SIZE = 1
+REBUILD_DATA = True
+
+# Retrieve and Augment Data
+data = emotions()
+
+if REBUILD_DATA:
+    # Retrieve Data
+    pass
+    # Augment Data
+
+else:
+    # Load Tensors
+    pass
+
+# Insert it in the DataLoader
+
+# Create the model and train/validate it
