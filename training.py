4e6862a4b4416d3892e118fdf852d9e27415b71e
Jason Buquiran <jbuq319@UoA.auckland.ac.nz>
2020-04-29 19:21:19 +1200

    Updated some stuff in the training.py
    -Made some more variables for the __init__
    
    -Added the load and save functionality to the training loop

--- training.py
+++ training.py
@@ -9,21 +9,25 @@ from tqdm import tqdm
 
 # A class that holds the necessary functions and variables to train the data
 class trainer:
-    def __init__(self, net, trainSet, validSet, testSet):
+    def __init__(self, net, trainSet, validSet, testSet, lr=0.001):
         self.net = net
         self.validSet = validSet
         self.trainSet = trainSet
         self.testSet = testSet
 
-    def startTrain(self, epoch, device, batch, lr=0.001):
+        # Initialise the optimiser and the loss function that is being used
+        self.optimiser = optim.Adam(self.net.parameters(), lr=lr)
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def startTrain(self, epoch, device, batch, load=False, fileName="-", saveFreq=20):
         pltLoss = plt.genLinePlot(title="Loss Analysis", ylabel="Loss", xlabel="Epoch", numOfLines=2,
                                   legendList=["train", "test"])
         pltAcc = plt.genLinePlot(title="Accuracy Analysis", ylabel="Accuracy", xlabel="Epoch", numOfLines=2,
                                  legendList=["train", "test"])
 
-        # Initialise the optimiser and the loss function
-        optimiser = optim.Adam(self.net.parameters(), lr=lr)
-        loss_func = nn.CrossEntropyLoss()
+        if load:
+            self.loadCheckpoint(fileName)
+            print("\n LOADED IN A NETWORK CHECKPOINT :" + fileName)
 
         # Start the iteration process
         for e in range(epoch):
@@ -37,10 +41,10 @@ class trainer:
                 # Training Algorithm
                 self.net.zero_grad()
                 output = self.net(batchImage)
-                loss = loss_func(output, batchLabel)
+                loss = self.loss_func(output, batchLabel)
                 lastLoss = loss.item()
                 loss.backward()
-                optimiser.step()
+                self.optimiser.step()
 
             # Validation Accuracy/Loss Calculation
             with torch.no_grad():
@@ -76,7 +80,7 @@ class trainer:
                     validOutputAcc = self.net(validImage)
 
                     for index, i in enumerate(validOutputAcc):
-                        validSum += loss_func(validOutputAcc, validLabel).item()
+                        validSum += self.loss_func(validOutputAcc, validLabel).item()
 
                         if torch.argmax(i) == validLabel[index]:
                             validCorrect += 1
@@ -93,6 +97,11 @@ class trainer:
             print("Current Loss (Train/Valid): " + str(trainLoss) + "/" + str(validLoss))
             print("Current Accuracy (Train/Valid): " + str(trainAcc) + "/" + str(validAcc) + "\n")
 
+            # Saving a Model at certain intervals of epoch and at the final epoch
+            if (e + 1) % saveFreq == 0 or e == (epoch - 1):
+                self.saveCheckpoint(e + 1, batch, fileName)
+                print("CURRENT MODEL HAS BEEN SAVED AS :" )
+
         testCorrect = 0
         testSum = 0
         matrix_plot = []
@@ -104,7 +113,7 @@ class trainer:
                 predicted = self.net(testImage)
 
                 for index, i in enumerate(predicted):
-                    testSum += loss_func(predicted, testLabel).item()
+                    testSum += self.loss_func(predicted, testLabel).item()
                     matrix_plot.append([torch.argmax(i), testLabel[index]])
 
                     if torch.argmax(i) == testLabel[index]:
@@ -125,18 +134,21 @@ class trainer:
         plt.showPlot(pltLoss, pltAcc)
         print(confusion_matrix)
 
-    def saveCheckpoint(self, model, optimizer, epoch: int, batchSize: int, fileName: str):
+    # Saves the progess to a fileName that was specified
+    def saveCheckpoint(self, epochCurrent: int, batchSize: int, fileName: str):
         checkpoint = {
-            "model_save": model.state_dict(),
-            "optimizer_save": optimizer.state_dict(),
-            "epoch_save": epoch,
+            "model_save": self.net.state_dict(),
+            "optimizer_save": self.optimiser.state_dict(),
+            "epoch_save": epochCurrent,
             "batchSize_save": batchSize
         }
 
-        torch.save(checkpoint, fileName
+        torch.save(checkpoint, fileName)
 
-    def loadModel(self, model, optimizer, checkpoint_path: str):
+    # Loads in the progress that was made
+    def loadCheckpoint(self, checkpoint_path: str):
         load_checkpoint = torch.load(checkpoint_path)
 
-        model.load_state_dict(load_checkpoint["model_save"])
-        optimizer.load_state_dict(load_checkpoint["optimizer_save"])
+        self.net.load_state_dict(load_checkpoint["model_save"])
+        self.optimiser.load_state_dict(load_checkpoint["optimizer_save"])
+
